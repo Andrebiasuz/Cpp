829a30fd95a9b909fc0c6f3b3fe6e62e2da60112
Andrebiasuz <58049577+Andrebiasuz@users.noreply.github.com>
2022-08-15 10:00:26 +0200

    Include calculator class using switch cases
    @version 1.2 - 15/08/2022 - A04 - Include calculator class using switch cases

--- WrKitsCourse/M2/M2_A02_Calculator.hpp
+++ WrKitsCourse/M2/M2_A02_Calculator.hpp
@@ -20,12 +20,13 @@ class Calculator_M02
     float substract(float num1, float num2);
     float divide(float num1, float num2) ;
     int modulo(int num1, int num2);
-    void setInputs(int num1, int num2, int num3, int num4, int num5, int num6);
-    void getResults();
+    void set6Inputs();
+    void set2Inputs();    
+    void getResultsA01computation();
     void lineEquation(int i1, int i2, int i3, int i4);
-
+    void GetSwitchResults();
 
     private:
-        int input1, input2,input3, input4, input5,input6;
+        int num1, num2, num3, num4, num5, num6;
         string calculatorTitle;
 };
