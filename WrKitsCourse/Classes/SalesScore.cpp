f64334b1b3d6660f3e0386b96d08d2d350b16d04
André Biasuz <58049577+Andrebiasuz@users.noreply.github.com>
2022-08-16 18:13:28 +0200

    M02_A05 - Added SetSoldProduct and GetSoldProduct
    M02_A05 - Added SetSoldProduct and GetSoldProduct in a very rudimentary form.

--- WrKitsCourse/Classes/SalesScore.cpp
+++ WrKitsCourse/Classes/SalesScore.cpp
@@ -1,3 +1,14 @@
+/**
+    SalesScore: A class who simulates a Enterprise system
+    @file SalesScore.cpp
+    @author André Biasuz, EE.
+    @version 1.0 - XX/08/2022
+    @version 1.1 - 16/08/2022 - M02_A05 - Added SetSoldProduct and GetSoldProduct in a very rudimentary form. 
+ 
+
+   ****Improvements to come: Improve reuseability of all member functions.
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
@@ -6,6 +17,9 @@ using namespace std;
 SalesScore::SalesScore(string title) // constructor
 {
     setStoreTitle(title);
+    prodA = 0;
+    prodB = 0;
+    prodC = 0;
 }
 
 // properties
@@ -35,4 +49,43 @@ void SalesScore::bootSystem(string storeTitle)
 
 // store as private the variable took from the set property. 
 string storeTitle;
+
+void SalesScore::setSoldProducts() {
+
+    int productCode;
+    
+    cout << "Enter sold product or Ctrl+d to finalize query : ";
+    while ( (productCode = cin.get()) != EOF)
+        {
+            
+            switch(productCode) {
+                case '1' :
+                    prodA++;
+                    break;
+                 case '2' :
+                    prodB++;
+                    break;      
+                 case '3' :
+                    prodC++;
+                    break;
+                case '\n' :
+                case '\t' :
+                case ' '  :
+                    break;
+                default:
+                    cout << "Please insert a valid product." << endl;
+            }
+        }
+    
+        SalesScore:getSoldProducts();
+}
+
+void SalesScore::getSoldProducts() {
+    cout << endl;
+    cout << "Products Sold:" << endl;
+    cout << "Code 1: " << prodA << " products" << endl;
+    cout << "Code 2: " << prodB << " products" << endl;
+    cout << "Code 3: " << prodC << " products" << endl;
+        
+}
 	
